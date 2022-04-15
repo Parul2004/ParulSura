79c3e77e5e9e5b46ebf3a63a627c05fb175c57c7
Parul2004 <parul1007.be21@chitkara.edu.in>
2022-04-15 07:28:13 -0700

    hello C++

---
+++ Addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2,sum;
+    // Here we are starting C++ 😁😁
+    cout<<"Enter first number"<<endl;
+    cin>>num1;
+    cout<<"Enter second number"<<endl;
+    cin>>num2;
+    sum = num1+num2;
+    cout<<"Sum of "<<num1<<" and "<<num2<<" is "<<sum<<endl;
+
+    return 0;
+}
