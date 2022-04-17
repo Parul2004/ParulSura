45377019e93c401c42bbfbdf409836c8471c41a2
Parul2004 <parul1007.be21@chitkara.edu.in>
2022-04-16 18:30:11 -0700

    files are added in github

---
+++ DataTypes.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+
+        Data types :- define a type of data that a variable can hold.
+        Data Types are catagoried into 3 groups:-
+        1. Built-in
+        2. User-defined
+        3. Derived
+
+        1. Built-in data types
+        a) int
+        b) float
+        c) char
+        d) double
+        e)boolean
+
+        2. User-defined data types
+        a) struct
+        b) union
+        c) enum
+
+        3. Dervied data types
+        a) Array
+        b) Pointer
+        c) Function
+
+
+    */
+
+    int a = 2, b = 3;
+    float c = 2.3;
+    char d = 'p';
+    bool e = true;
+    double f = 2.3;
+
+    cout << "Value of a is " << a << endl;
+    cout << "Value of b is " << b << endl;
+    cout << "Value of c is " << c << endl;
+    cout << "Value of d is " << d << endl;
+    cout << "Value of e is " << e << endl;
+    cout << "Value of f is " << f << endl;
+
+    /*
+        the difference in float and double is double can cotain more number of decimals values as compare to float.
+
+        Boolean Value :- true = 1 & false = 0.
+        
+    */
+    return 0;
+}
---
+++ VariablesAndComments.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+
+    Variables are basically a type of container which is used to store data
+    for example if we write :- int a = 3;
+    here "int" is a datatype which we will discuss later
+    "a" is a variable which is used to store 3 (a number)
+
+    */
+
+    // this is a sinle line comment
+
+    /*
+    this is a
+    multilin comment
+
+    "Comments are ignored by the compilier"
+    */
+
+    int a = 4;
+    cout<<"hello parul"<<endl;
+    cout << a << endl;
+
+    /*
+here cout is used to print 
+endl is a newline chracter
+    */
+    return 0;
+}
