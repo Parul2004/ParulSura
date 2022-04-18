2272fa2a190b2fe67fb1758623ecc3a9d2f705f5
Parul2004 <parul1007.be21@chitkara.edu.in>
2022-04-17 22:00:06 -0700

    if-else statments

---
+++ if-else.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+        if-else statements
+    */
+    int age;
+    cout << "beta kirpya aapne aayu btao" << endl;
+    cin>>age;
+    if (age>18)
+    {
+        cout<<"Jo chaho vo kro full chut h tumari 🙃🙃"<<endl;
+    }
+    else if (age<18)
+    {
+        cout<<"Abhi bache ho tum sabr kro thoda 😁😁"<<endl;
+    }
+    else
+    {
+        cout<<"Tumhari bare me kya he bolu m padhae pe dyan do ya sab mo maya h balak 😂😂"<<endl;
+    }
+
+    return 0;
+}
